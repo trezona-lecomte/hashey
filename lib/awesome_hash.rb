ab56e77be9ce2780e70198b716acc34455de2ff1
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-23 11:55:37 +1200

    Revert "Add crude automatic re-distribution"
    This reverts commit f8cd0fbf0c801d09aa595cda8466af5f8f9d1131.

--- lib/awesome_hash.rb
+++ lib/awesome_hash.rb
@@ -1,55 +1,29 @@
 class AwesomeHash
-  attr_reader :buckets, :entry_count
+  attr_reader :values
 
   def initialize
-    @buckets = Array.new(16, [])
-    @entry_count = 0
+    @values = []
   end
 
   def []=(key, value)
-    @entry_count += 1
+    index = hash(key)
 
-    if (@entry_count.to_f / @buckets.size) > 0.75
-      redistribute
+    if @values[index] == nil
+      @values[index] = []
     end
 
-    index = hash(str: key)
-
-    @buckets[index] << [key, value]
+    @values[index] << [key, value]
   end
 
   def [](key)
-    target_bucket = @buckets[hash(str: key)]
+    target_bucket = @values[hash(key)]
 
-    target_entry = target_bucket.find { |k, v| k == key }
+    target_pair = target_bucket.find { |k, v| k == key }
 
-    target_entry[1]
+    target_pair[1]
   end
 
-  def distribution_info
-    output = "Number of buckets: #{@buckets.size}\n"
-    output += "Number of empty buckets: #{num_of_empty_buckets}\n"
+  def hash(str)
+    str.chars.map(&:ord).reduce(:+)
   end
-
-  def num_of_empty_buckets
-    @buckets.count { |bucket| bucket.empty? }
-  end
-
-  private
-    def hash(str:, num_of_buckets: @buckets.size)
-      str.sum % num_of_buckets
-    end
-
-    def redistribute
-      new_buckets = Array.new(@buckets.size * 2, [])
-
-      @buckets.each do |bucket|
-        bucket.each do |entry|
-          index = hash(str: entry[0], num_of_buckets: new_buckets.size)
-          new_buckets[index] << [entry[0], entry[1]]
-        end
-      end
-
-      @buckets = new_buckets
-    end
 end
